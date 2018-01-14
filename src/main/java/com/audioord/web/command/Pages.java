186872dd5d5376b2025b4bad7c2c04398988204f
OlegSavik <oleg.savik1996@gmail.com>
2018-01-15 01:09:07 +0300

    fix 404 page path

--- src/main/java/com/audioord/web/command/Pages.java
+++ src/main/java/com/audioord/web/command/Pages.java
@@ -5,7 +5,7 @@ public final class Pages {
   public static final String INDEX_PAGE = "/index.jsp";
   public static final String SIGN_IN_PAGE = "/pages/SignIn.jsp";
   public static final String SIGN_UP_PAGE = "/pages/SignUp.jsp";
-  public static final String NOT_FOUND_PAGE = "/pages/SignUp.jsp";
+  public static final String NOT_FOUND_PAGE = "/404.jsp";
 
   private Pages() {
     super();
