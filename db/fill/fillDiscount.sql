dfc523dbf2f270b5ac993f6ba5e82288e7531186
OlegSavik <oleg.savik1996@gmail.com>
2018-01-21 00:04:54 +0300

    fix db scripts

--- db/fill/fillDiscount.sql
+++ db/fill/fillDiscount.sql
@@ -1,6 +1,5 @@
-insert into Discount(Percent, DateBegin,DateEnd)values(0,'2018-01-01','2018-02-01');
-insert into Discount(Percent, DateBegin,DateEnd)values(2,'2018-02-07','2018-03-12');
-insert into Discount(Percent, DateBegin,DateEnd)values(3,'2018-03-02','2018-04-17');
-insert into Discount(Percent, DateBegin,DateEnd)values(4,'2018-04-03','2018-05-23');
-insert into Discount(Percent, DateBegin,DateEnd)values(5,'2018-05-04','2018-06-18');
-
+INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (0, '2018-01-01', '2018-02-01');
+INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (2, '2018-02-07', '2018-03-12');
+INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (3, '2018-03-02', '2018-04-17');
+INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (4, '2018-04-03', '2018-05-23');
+INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (5, '2018-05-04', '2018-06-18');
--- db/fill/fillFeedbackTable.sql
+++ db/fill/fillFeedbackTable.sql
@@ -1,9 +1,9 @@
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Super track! Want to buy it'       , 1,1);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Best service ever...'              , 9,2);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Damn, great song'                   ,33,5);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Share it'                           ,37,6);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('I try not to cry...'                ,38,6);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Mozart- is a genious'               ,44,7);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Love it'                            ,16,3);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Oh My God, I buy it for a million'  ,10,2);
-insert into TrackFeedback(Comment, IdTrack,IdUser)values('Hell Yeah!!!'                       ,26,4);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Super track! Want to buy it', 1, 1);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Best service ever...', 9, 2);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Damn, great song', 33, 5);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Share it', 37, 6);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('I try not to cry...', 38, 6);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Mozart- is a genious', 44, 7);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Love it', 16, 3);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Oh My God, I buy it for a million', 10, 2);
+INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Hell Yeah!!!', 26, 4);
