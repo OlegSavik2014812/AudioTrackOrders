7bc317c99b69850026415ccd87417919341a3335
OlegSavik <oleg.savik1996@gmail.com>
2018-01-30 23:07:35 +0300

    cleanup db creation

--- db/fill/fillDiscount.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (0, '2018-01-01', '2018-02-01');
-INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (2, '2018-02-07', '2018-03-12');
-INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (3, '2018-03-02', '2018-04-17');
-INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (4, '2018-04-03', '2018-05-23');
-INSERT INTO Discount (Percent, DateBegin, DateEnd) VALUES (5, '2018-05-04', '2018-06-18');
--- db/fill/fillFeedbackTable.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Super track! Want to buy it', 1, 1);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Best service ever...', 9, 2);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Damn, great song', 33, 5);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Share it', 37, 6);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('I try not to cry...', 38, 6);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Mozart- is a genious', 44, 7);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Love it', 16, 3);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Oh My God, I buy it for a million', 10, 2);
-INSERT INTO TrackFeedback (Comment, IdTrack, IdUser) VALUES ('Hell Yeah!!!', 26, 4);
