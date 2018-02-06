6dcdd3fbebcc2f0763a28945c331cfa572194c7f
OlegSavik <oleg.savik1996@gmail.com>
2018-02-06 11:20:43 +0300

    cleanup

--- src/main/java/com/audioord/web/command/Commands.java
+++ src/main/java/com/audioord/web/command/Commands.java
@@ -24,7 +24,7 @@ import java.util.Map;
  */
 public final class Commands {
 
-  private static final Map<String, Command> COMMAND_MAP = define(new HashMap<>());
+  private static final Map<String, Command> COMMAND_MAP = define(new HashMap<String, Command>());
   private static final NoCommand NO_COMMAND = new NoCommand();
 
   private Commands() {
